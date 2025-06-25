ad7c709ab47604fc068374477499cc338d60e616
Grace Obanegha <graceobanegha2@gmail.com>
2025-06-25 15:04:10 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+function calculateLove() {
+    const name1 = document.getElementById('name1').value.trim();
+    const name2 = document.getElementById('name2').value.trim();
+
+    if (!name1 || !name2) {
+        document.getElementById('result').innerText = "Please enter both names!";
+        return;
+        }
+         const compatibility = Math.floor(Math.random() * 101);
+        document.getElementById('result').innerText = `${name1} ❤️ ${name2} = ${compatibility}% compatibility!`;
+        }
+
+    // if (!name1 || !name2) {
+    //     resultDiv.innerText = "Please enter both names.";
+    //     return;
+    // }
+
